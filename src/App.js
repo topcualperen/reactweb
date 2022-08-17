42dcbc652840dc6cbf7337aeca5cc1669cdfd35b
Alperen Topcu <alperenttpc78@gmail.com>
2022-08-17 12:05:34 +0300

    App.js şimdilik yorum satırı yapıldı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
-function App() {
-  return (
+//function App() {
+  //return (
+    /*
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +21,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
+    */
+  //);
+//}
 
-export default App;
+//export default App;
