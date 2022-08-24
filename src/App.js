d9efeef1a4796eaccf9254fa3bfca97bea0e4f80
Alperen Topcu <alperenttpc78@gmail.com>
2022-08-24 15:01:03 +0300

    childPage update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import Navbar from "./components/Navbar";
 import MainPage from "./pages/MainPage/MainPage";
-
 import LoginPage from './pages/LoginPage/LoginPage';
 import WalletCard from './wallet-connection/WalletCard';
 
