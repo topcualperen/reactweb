ba110755deeb7beeb74e8f6397509194a302a10a
Alperen Topcu <alperenttpc78@gmail.com>
2022-08-25 17:16:46 +0300

    Childpageinfo fixed

--- src/pages/ChildPage/ChildPage.js
+++ src/pages/ChildPage/ChildPage.js
@@ -7,7 +7,9 @@ export default function ChildPage(){
    
         <div class="main">
           <Menu />
+          
           <ChildPageInfo />
+          
           
         </div>
    
--- src/pages/ChildPage/ChildPageInfo.js
+++ src/pages/ChildPage/ChildPageInfo.js
@@ -1,46 +1,102 @@
-import * as React from 'react';
-import "./ChildPage.css"
-import { Table } from "antd";
-
-
-
-const columns = [
-  { 
-    dataIndex: 'lastName', 
-   name: 'Last name', 
-   width: 130
- },
-  {
-    dataIndex: 'age',
-    name: 'Age',
-    width: 290,
-  },
-  {
-    dataIndex: 'fullName',
-    name: 'Full name',
-    width: 290,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+import { useEffect, useState } from "react";
+import "./ChildPage.css";
+import { Button, Table, Form, Input, Modal } from "antd";
 
 export default function DataTable() {
+  const [isWithdrawStateOpen, setIsWithdrawStateOpen] = useState(false);
+  const [selectedChild, setSelectedChild] = useState();
+  const [rows, setRows] = useState();
+
+  const handleWithdraw = (data) => {
+    console.log("gelen:", {data, selectedChild});
+    //contracta gönder await
+
+    setIsWithdrawStateOpen(false);
+  };
+
+  const handleAddChild = (child) => {
+    console.log("cocuk ekle:", child);
+    //tx = await contract...
+    //await tx.wait()
+
+    setRows((prev) => {
+      return [...prev, child]
+    })
+  }
+
+  useEffect(() => {
+    const children = [
+      { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+      { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+      { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+      { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+      { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 0 },
+      { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+      { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+      { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+      { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    ];
+
+    setRows(children);
+  }, [])
+
+  const columns = [
+    {
+      dataIndex: "lastName",
+      title: "Last name",
+    },
+    {
+      dataIndex: "age",
+      title: "Age",
+    },
+    {
+      dataIndex: "fullName",
+      title: "Full name",
+    },
+    {
+      width: 1,
+      render: (_, row) => (
+        <Button onClick={() => console.log(row.lastName)}>Yatır</Button>
+      ),
+    },
+    {
+      width: 1,
+      render: (_, row) => (
+        <Button onClick={() => {
+          setSelectedChild(row.lastName)
+          setIsWithdrawStateOpen(true)
+        }}>Çek</Button>
+      ),
+    },
+  ];
+
   return (
-    <div style={{ height: 400, width: '100%'}}>
-      {/* <Table
-        dataSource={rows} 
+    <div style={{ height: 400, width: "100%", padding: "35px" }}>
+      <Table
+        className="deneme-class"
+        dataSource={rows}
         columns={columns}
-      /> */}
+        pagination={{ pageSize: 6 }}
+      />
+
+      <Button onClick={() => handleAddChild({lastName: "asdt"})}>Ekle</Button>
+
+      <Modal
+        title="Basic Modal"
+        visible={isWithdrawStateOpen}
+        footer={null}
+        // onOk={handleOk}
+        onCancel={() => setIsWithdrawStateOpen(false)}
+      >
+        <Form onFinish={handleWithdraw} layout="vertical">
+          <Form.Item label="Yatırılacak Miktar" name="value">
+            <Input type="number"/>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">Çek</Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 }
