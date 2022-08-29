99b75bf8a82515bcb557b69230be353a1d2b23a8
Ece Güz <eguz18@ku.edu.tr>
2022-08-29 14:48:08 +0300

    child info commit

--- src/pages/ChildMainPage/ChildInfo.js
+++ src/pages/ChildMainPage/ChildInfo.js
@@ -2,14 +2,29 @@ import React from "react"
 import LocalAtmIcon from '@mui/icons-material/LocalAtm';
 import Button from '@mui/material/Button';
 import "./ChildMainPage.css"
+import { getContract } from "../../wallet-connection/WalletCard";
+import {useEffect,useState} from "react";
+import {ethers} from  "ethers";   
 
 
 
 export default function ChildInfo() {
+    let [childBalance, setChildBalance] = useState();
+    const getChildBalance = async () => {
+        const contract = await getContract();
+        const child = await contract.provider.getBalance("0x3C926421417bc23AB0A350aDCd516b1bcCd7d242");
+        setChildBalance(ethers.utils.formatEther(child));
+        console.log( "balance of child ",child);
+    }
+    useEffect(()=> {
+        getChildBalance();
+    },[]);
+    
     return (
     <div className="container">
     <h2>Çekilebilecek Miktar</h2>
     <input type="text" ></input>
+    <p>{childBalance}</p>
     <h2>Devir Tarihi</h2>
     <input type="text" ></input>
     <Button variant="contained" startIcon={<LocalAtmIcon />} className="container_button">
