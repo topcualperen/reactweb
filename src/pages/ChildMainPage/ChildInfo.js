41c4d04464ae8a946d017b90ebf1ede2c9672b56
Ece Güz <eguz18@ku.edu.tr>
2022-08-29 15:21:27 +0300

    can observe the child balance

--- src/pages/ChildMainPage/ChildInfo.js
+++ src/pages/ChildMainPage/ChildInfo.js
@@ -10,23 +10,35 @@ import {ethers} from  "ethers";
 
 export default function ChildInfo() {
     let [childBalance, setChildBalance] = useState();
+    let [childWithdrawDate, setChildWithdrawDate] = useState();
     const getChildBalance = async () => {
         const contract = await getContract();
         const child = await contract.provider.getBalance("0x3C926421417bc23AB0A350aDCd516b1bcCd7d242");
         setChildBalance(ethers.utils.formatEther(child));
-        console.log( "balance of child ",child);
+        console.log("balance of child ",child);
     }
     useEffect(()=> {
         getChildBalance();
     },[]);
-    
+
+    // const getChildWithdrawDate = async () => {
+    //     const contract = await getContract();
+    //     //contracttan nasıl çekeceğimizi bilemedim
+    //     const withdrawDate = await contract.withdrawDate;
+    //     setChildWithdrawDate(withdrawDate);
+    //     console.log("child withdraw date: ", withdrawDate);
+    // }
+    // useEffect(()=>{
+    //     getChildWithdrawDate();
+    // },[]);
+
     return (
     <div className="container">
     <h2>Çekilebilecek Miktar</h2>
-    <input type="text" ></input>
-    <p>{childBalance}</p>
+
+    <p className="child-info">{childBalance}</p>
     <h2>Devir Tarihi</h2>
-    <input type="text" ></input>
+    {/* <p className="child-info"> {childWithdrawDate}</p> */}
     <Button variant="contained" startIcon={<LocalAtmIcon />} className="container_button">
         Yatırımı Çek
     </Button>
